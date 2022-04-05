09f1adad6b4de56f93ffd5dda52a935a520df9e3
Pallavi679 <pallavi.pandey_19@sakec.ac.in>
2022-04-05 17:10:23 +0530

    final commit

--- src/app/home/events/events.component.ts
+++ src/app/home/events/events.component.ts
@@ -12,7 +12,12 @@ export class EventsComponent implements OnInit {
   uData:Upcoming[]
   eData:Event[]
   flag:boolean
-
+  upDesc;
+  upTitle;
+  upLink;
+  upId;
+  upImage;
+  regEnabled = false;
   constructor(private dataService: DatahandlerService) {
     
       this.dataService.setTitle('E-Cell Events');
@@ -28,10 +33,27 @@ export class EventsComponent implements OnInit {
         this.flag = false;
       }
     });
+    
 
     this.dataService.getEvents().subscribe((data)=>{
       this.eData = data
     });
+    this.dataService.getUpcoming().subscribe((data)=>{
+      this.uData = data
+      this.upId = data[0].id;
+      this.upTitle = data[0].title;
+      this.upDesc = data[0].description;
+      this.upImage = data[0].image;
+      if(this.uData[0].link === 'website'){
+        this.regEnabled = true;
+      }
+      else{
+        this.upLink = data[0].link
+      }
+    });
+
+  
   }
+  
 
 }
